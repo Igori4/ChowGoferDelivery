f55c5e14b3aba7e44d9e6c36cd1dcbb7a205bf2e
Igori4 <igorpilyak@gmail.com>
2019-07-21 02:07:00 +0300

    add new duaration to animation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 let singInForm = document.getElementById('registrationContainer');
 let singInButton = document.getElementById('buttonForSignIn');
-singInButton.onclick = function(){
-    singInForm.hidden = false;
-}
-
 let closeRegistration = document.querySelector('.close-registration-menu');
 
 
-closeRegistration.addEventListener('click',() => {
-    singInForm.hidden = true;
-})
+singInButton.addEventListener('click', show)
+
+function show() {
+  singInForm.hidden = false;
+}
+
+function hide() {
+  singInForm.hidden = true;
+}
+
+
+closeRegistration.addEventListener('click', hide)
 
